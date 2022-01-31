7472b305d21c7f813a2ff59bdb24babaf958e253
Sam Williams <samwilliams281@gmail.com>
2022-01-31 17:41:05 -0500

    add playerName func

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,14 @@
-window.alert("This is an alert! Javascript is running!")
+
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun");
+}
+var playerName = window.prompt("What is your robot's name?");
+// note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourslef a message");
+console.log(10 + 10)
+console.log("Our robot's name is " + playerName)
+// fight();
