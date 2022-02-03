bd4fef3bf2e74dc15aa2e25a771c9bb346c69d34
Sam Williams <samwilliams281@gmail.com>
2022-02-03 16:14:09 -0500

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -68,8 +68,13 @@ var fight = function(enemyName) {
     }
   }
 };
-
-// fight each enemy-robot by looping over them and fighting them one at a time
+// function to start a new game
+var startGame = function() {
+  // reset player stats
+  playerHealth = 100;
+  playerAttack = 10;
+  playerMoney = 10;
+  // fight each enemy-robot by looping over them and fighting them one at a time
 for (var i = 0; i < enemyNames.length; i++) {
   // if player is still alive, keep fighting
   if (playerHealth > 0) {
@@ -93,4 +98,33 @@ for (var i = 0; i < enemyNames.length; i++) {
     window.alert('You have lost your robot in battle! Game Over!');
     break;
   }
-}
+  endGame();
+ };
+
+//  play again
+startGame();
+};
+
+// function to end the entire game
+var endGame = function() {
+  window.alert("The game has now ended. Lets see how you did!");
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+    window.alert("You've lost your robot in battle.");
+  }
+  // ask player if they'd like to play again
+  var playAgainConfirm = window.confirm("Would you like to play again?");
+
+  if (playAgainConfirm) {
+    // restart game 
+    startGame();
+  }
+  
+  else {
+    window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+  }
+};
+
+// start game when the page loads
+startGame();
